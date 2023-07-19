185f1b9782e91824cf8cdd5bfe94ed343ed826f4
Jason Standfast <45206666+jstandfast@users.noreply.github.com>
2023-07-19 19:36:47 -0400

    Set Up Basic Web Pages
    Login, Sign up and a generic html template established for later adjustment and use.

--- code_tracer/code_tracer/ui/views.py
+++ code_tracer/code_tracer/ui/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 # Create your views here.
 def home(request):
     return render(request, 'ui/home.html')
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'ui/signup.html'
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')  # Replace 'home' with the view you want to redirect to after successful signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'ui/signup.html', {'form': form})
